12fd1ed4563491dc9c03f66c2a182582f1ab7f8e
Victor Wardi <victorcaetano@live.com>
2018-09-04 14:29:37 -0300

    Ading index Controller and page

---
+++ src/main/java/guru/spring/recipe/recipespring/controllers/IndexController.java
@@ -0,0 +1,28 @@
+package guru.spring.recipe.recipespring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Victor Wardi - @vwardi - on 04/09/2018.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public  String getIndexPage(Model model){
+         model.addAttribute("appName", "My Recipes APP");
+
+        List<String> recipes = Arrays.asList("Orange Cake", "Chocolate Cookie", "Strawberry Jam", "Baked Potatoes");
+        model.addAttribute("recipes", recipes);
+
+
+        return "index";
+    }
+
+}
